4fc755790e449fca3a8a4523de710fff1e760bcf
andreglez <gonzalez.andre@uabc.edu.mx>
2024-10-05 17:02:33 -0700

    Main menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-// src/App.js
-import React from 'react';
-import StarField from './components/StarField';
+import React, { useState } from 'react';
+import MainMenu from './components/MainMenu';
+import StarField from './components/StarField'; // Ejemplo de otro componente
 
 function App() {
+  const [inGame, setInGame] = useState(false);
+
   return (
-    <div className="App">
-      <StarField />
+    <div>
+      {!inGame ? (
+        <MainMenu />
+      ) : (
+        <StarField /> // Cambia a tu componente principal del juego
+      )}
     </div>
   );
 }
