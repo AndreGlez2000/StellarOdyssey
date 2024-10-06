1ba58a0eac75d176ea79bccc42598c49a4e84a1d
andreglez <gonzalez.andre@uabc.edu.mx>
2024-10-05 21:23:07 -0700

    commit antes de cagarla macizo

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ import StarField from './components/StarField'; // Ejemplo de otro componente
 function App() {
   const [inGame, setInGame] = useState(false);
 
+  // Función para iniciar el juego
+  const startGame = () => {
+    setInGame(true); // Cambia el estado para iniciar el juego
+  };
+
   return (
     <div>
       {!inGame ? (
-        <MainMenu />
+        // Pasar la función startGame como prop a MainMenu
+        <MainMenu onStart={startGame} />
       ) : (
         <StarField /> // Cambia a tu componente principal del juego
       )}
--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -2,12 +2,12 @@
 import React from 'react';
 import './MainMenu.css'; // Importa los estilos
 
-const MainMenu = () => {
+const MainMenu = ({ onStart }) => {
   return (
     <div className="main-menu">
       <h1 className="title">Stellar Odyssey</h1>
       <div className="menu-options">
-        <button className="menu-button">Start Journey</button>
+        <button className="menu-button" onClick={onStart}>Start Journey</button>
       </div>
     </div>
   );
